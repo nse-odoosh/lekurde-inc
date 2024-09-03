ff02d00e81e759da227dba148dc08a6820dcd9c7
Nicolas Seinlet <nicolas@seinlet.com>
2024-09-03 09:47:24 +0200

    [IMP] manage spawn rate

--- load_test/lt.sh
+++ load_test/lt.sh
@@ -2,20 +2,33 @@
 if [ -z "$4" ]
   then
       echo "No argument supplied"
-      echo "lt.sh test_file nbr_workers concurrency time [rounds]"
+      echo "lt.sh test_file nbr_workers concurrency time [rounds] [spawn_rate]"
       exit 1
 fi
 
 nbr_rounds=1
-if [ -z "$5" ]
+if [ -n "$5" ]
   then
     nbr_rounds=$5
 fi
+if [[ $nbr_rounds -le 0 ]]
+  then
+    nbr_rounds=1
+fi
 
-num_test=0
+spawn_rate=1
+if [ -n "$6" ]
+  then
+    spawn_rate=$6
+fi
+if [[ $spawn_rate -le 0 ]]
+  then
+    spawn_rate=1
+fi
+
+num_test=1
 while [[ $num_test -le $nbr_rounds ]]
 do
-    num_test=$(( $num_test + 1 ))
     # Launch required number of workers
     x=1
     while [[ $x -le $2 ]]
@@ -24,5 +37,6 @@ do
         x=$(( $x + 1 ))
     done
     # Launch master and collect datas in csv
-    locust -f $1 --headless --users $3 --spawn-rate $3 --run-time $4m --master --expect-workers=$2 --csv=results/$2-$3-$num_test
+    locust -f $1 --headless --users $3 --spawn-rate $spawn_rate --run-time $4m --master --expect-workers=$2 --csv=results/$2-$3-$num_test
+    num_test=$(( $num_test + 1 ))
 done
